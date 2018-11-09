d6e5c07d48122aefdf7162e65598b46bba48348a
362963896 <zephyru@163.com>
2018-11-09 06:43:56 +0000

    重构下载模块

---
+++ server/service/downloadImg.back.js
@@ -0,0 +1,124 @@
+/**
+*  下载模块 v 0.1
+*  功能：根据url下载文件到指定文件夹 
+*        
+**/
+const servicePath = '../';
+const manPath = '../../';
+
+const pixivAbout = require(manPath + 'config')['pixivConfig'];
+const pathConfig = require(manPath + 'config')['pathConfig'];
+
+const requireMehod = require(servicePath + 'router/refPath.js');
+const request = requireMehod('request');
+const fs = requireMehod('fs');
+const parseUrl = requireMehod('url');
+const checkImg = requireMehod('checkImg');
+const events = requireMehod('events');
+
+var emitter = new events.EventEmitter();
+
+
+var wait = 30000;
+var waitTimer = null;
+
+
+function downLoadMethod(url, upPath=pathConfig.downloadPath) {
+
+    var urlObj = parseUrl.parse(url);
+    var fileName = urlObj.path.slice(urlObj.path.lastIndexOf('/'));
+    var imgPath = upPath+ fileName;
+
+    if (!fs.existsSync(upPath)){
+        fs.mkdirSync(upPath)
+    }
+
+
+    var promise = new Promise((resolve, reject) => {
+        var Option = {
+            mainDownloadEnd: resolve,
+            headers: pixivAbout.headers,
+            url: url,
+            fileName: fileName,
+            imgPath: imgPath,
+            runNum: 1
+        }
+        if (checkImg(imgPath)) {
+            console.log('downLoadImg:文件 ' + fileName + ' 存在且已经下载完全.');
+            emitter.emit('downOver', Option);
+        } else {
+            downMethod(Option);
+        }
+
+
+    });
+    return promise;
+}
+
+function downMethod(Option) {
+    if (waitTimer != null) {
+        global.clearTimeout(waitTimer);
+        waitTimer = null;
+    }
+    if (Option.runNum === 1) {
+        wait = 5000;
+    }
+
+    var fileName = Option.fileName;
+    var imgPath = Option.imgPath;
+    var requresOpt = {
+        url: Option.url,
+        headers: Option.headers
+    }
+    var stream = fs.createWriteStream(Option.imgPath);
+    var downRequest = request(requresOpt);
+    downRequest.catch(() => {
+        console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误');
+        emitter.emit('tryAgain', Option);
+    });
+    var pipe = downRequest.pipe(stream);
+    pipe.on('error', e => {
+        console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误：', e);
+        emitter.emit('tryAgain', Option);
+
+    });
+    pipe.on('finish', () => {
+        if (checkImg(imgPath)) {
+            console.log('downLoadImg:文件 ' + fileName + '下载完成！下载次数：' + Option.runNum);
+            emitter.emit('downOver', Option);
+        } else {
+            console.log('downLoadImg:文件 ' + fileName + '下载失败，文件不完全');
+            emitter.emit('tryAgain', Option);
+
+        }
+
+    });
+}
+emitter.on('downOver', (Option) => {
+    Option.mainDownloadEnd(Option);
+});
+
+emitter.on('tryAgain', (Option) => {
+    console.log('downLoadImg：进入重试流程，等待时间，', wait / 1000, 's');
+
+    if (Option.runNum < 4) {
+        if (waitTimer === null) {
+            waitTimer = global.setTimeout(() => {
+                Option.runNum++;
+                console.log('downLoadImg:文件 ' + Option.fileName + '尝试第：' + Option.runNum + '次重下');
+                downMethod(Option);
+
+                wait = wait + wait;
+            }, wait);
+        } else {
+            console.log('downLoadImg:文件 ' + Option.fileName + '已经创建等待任务');
+        }
+
+    } else {
+        console.log('downLoadImg:文件下载失败，已尝试：' + Option.runNum + '次重下');
+        Option.downState = 'faill'
+        Option.mainDownloadEnd(Option);
+    }
+
+});
+module.exports = downLoadMethod;
--- server/service/downloadImg.js
+++ server/service/downloadImg.js
@@ -1,5 +1,5 @@
 /**
-*  下载模块 v 0.1
+*  下载模块 v 0.2
 *  功能：根据url下载文件到指定文件夹 
 *        
 **/
@@ -14,111 +14,137 @@ const request = requireMehod('request');
 const fs = requireMehod('fs');
 const parseUrl = requireMehod('url');
 const checkImg = requireMehod('checkImg');
-const events = requireMehod('events');
 
-var emitter = new events.EventEmitter();
+const pathCash = {}
+pathCash[pathConfig.downloadPath] = true;
 
-
-var wait = 30000;
-var waitTimer = null;
-
-
-function downLoadMethod(url, upPath=pathConfig.downloadPath) {
-
-    var urlObj = parseUrl.parse(url);
-    var fileName = urlObj.path.slice(urlObj.path.lastIndexOf('/'));
-    var imgPath = upPath+ fileName;
-
-    if (!fs.existsSync(upPath)){
-        fs.mkdirSync(upPath)
-    }
-
-
-    var promise = new Promise((resolve, reject) => {
-        var Option = {
-            mainDownloadEnd: resolve,
-            headers: pixivAbout.headers,
-            url: url,
-            fileName: fileName,
-            imgPath: imgPath,
-            runNum: 1
-        }
-        if (checkImg(imgPath)) {
-            console.log('downLoadImg:文件 ' + fileName + ' 存在且已经下载完全.');
-            emitter.emit('downOver', Option);
-        } else {
-            downMethod(Option);
-        }
-
-
-    });
+function downLoadMethod(url, upPath = pathConfig.downloadPath) {
+    const newDownObj = new downLoadClass({
+        downFrom: 'pixiv',
+        downToPath: upPath,
+        wait: 5000,
+        tryNum: 3
+    })
+    let promise = newDownObj.start(url);
     return promise;
 }
 
-function downMethod(Option) {
-    if (waitTimer != null) {
-        global.clearTimeout(waitTimer);
-        waitTimer = null;
-    }
-    if (Option.runNum === 1) {
-        wait = 5000;
-    }
 
-    var fileName = Option.fileName;
-    var imgPath = Option.imgPath;
-    var requresOpt = {
-        url: Option.url,
-        headers: Option.headers
+class downLoadClass {
+    constructor({
+        downFrom = 'pixiv',
+        downToPath = pathConfig.downloadPath,
+        wait = 5000,
+        tryNum = 3
+    }) {
+        this.downToPath = downToPath;
+        this.downFrom = 'pixiv'; //TODO 支持其它图站的下载
+        this.tryAgainConfig = {
+            wait: wait,
+            tryNum: tryNum,
+        }
     }
-    var stream = fs.createWriteStream(Option.imgPath);
-    var downRequest = request(requresOpt);
-    downRequest.catch(() => {
-        console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误');
-        emitter.emit('tryAgain', Option);
-    });
-    var pipe = downRequest.pipe(stream);
-    pipe.on('error', e => {
-        console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误：', e);
-        emitter.emit('tryAgain', Option);
-
-    });
-    pipe.on('finish', () => {
-        if (checkImg(imgPath)) {
-            console.log('downLoadImg:文件 ' + fileName + '下载完成！下载次数：' + Option.runNum);
-            emitter.emit('downOver', Option);
-        } else {
-            console.log('downLoadImg:文件 ' + fileName + '下载失败，文件不完全');
-            emitter.emit('tryAgain', Option);
-
+    start(url) {
+        let downObj = this;
+        let urlObj = parseUrl.parse(url);
+        let fileName = urlObj.path.slice(urlObj.path.lastIndexOf('/'));
+        let imgPath = downObj.downToPath + fileName;
+        if (!pathCash[downObj.downToPath] && !fs.existsSync(downObj.downToPath)) {
+            fs.mkdirSync(downObj.downToPath)
+            pathCash[downObj.downToPath] = true;
         }
 
-    });
-}
-emitter.on('downOver', (Option) => {
-    Option.mainDownloadEnd(Option);
-});
+        let promise = new Promise((resolve, reject) => {
 
-emitter.on('tryAgain', (Option) => {
-    console.log('downLoadImg：进入重试流程，等待时间，', wait / 1000, 's');
+            let Option = {
+                mainDownloadEnd: resolve,
+                headers: pixivAbout.headers,
+                url: url,
+                fileName: fileName,
+                imgPath: imgPath,
+                runNum: 1,
+            }
+            if (checkImg(imgPath)) {
+                console.log('downLoadImg:文件 ' + fileName + ' 存在且已经下载完全.');
+                downObj.downOver(Option);
+            } else {
+                downObj.downMethod(Option);
+            }
 
-    if (Option.runNum < 4) {
-        if (waitTimer === null) {
-            waitTimer = global.setTimeout(() => {
-                Option.runNum++;
-                console.log('downLoadImg:文件 ' + Option.fileName + '尝试第：' + Option.runNum + '次重下');
-                downMethod(Option);
 
-                wait = wait + wait;
-            }, wait);
-        } else {
-            console.log('downLoadImg:文件 ' + Option.fileName + '已经创建等待任务');
+        });
+        return promise;
+    }
+    downMethod(Option) {
+        let downObj = this;
+        if (Option.waitTimer != null) {
+            global.clearTimeout(Option.waitTimer);
+            Option.waitTimer = null;
         }
 
-    } else {
-        console.log('downLoadImg:文件下载失败，已尝试：' + Option.runNum + '次重下');
-        Option.downState = 'faill'
+        let fileName = Option.fileName;
+        let imgPath = Option.imgPath;
+        let requresOpt = {
+            url: Option.url,
+            headers: Option.headers
+        }
+        let stream = fs.createWriteStream(Option.imgPath);
+        let downRequest = request(requresOpt);
+        downRequest.catch(() => {
+            console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误');
+            downObj.tryAgain(Option);
+        });
+        let pipe = downRequest.pipe(stream);
+        pipe.on('error', e => {
+            console.log('downLoadImg:文件 ' + fileName + '下载失败，发生错误：', e);
+            downObj.tryAgain(Option);
+        });
+        pipe.on('finish', () => {
+            if (checkImg(imgPath)) {
+                console.log('downLoadImg:文件 ' + fileName + '下载完成！下载次数：' + Option.runNum);
+                downObj.downOver(Option);
+            } else {
+                console.log('downLoadImg:文件 ' + fileName + '下载出错,文件不完全');
+                if (Option.runNum === 1) {
+                    downObj.tryAgain(Option);
+                } else {
+                    console.log('downLoadImg:文件 ' + fileName + '下载完成,但是可能文件不完全!下载次数:' + Option.runNum);
+                    Option.downState = 'incomplete'
+                    Option.mainDownloadEnd(Option);
+                }
+
+            }
+
+        });
+    }
+    downOver(Option) {
+        Option.state = 'sucess'
         Option.mainDownloadEnd(Option);
     }
+    tryAgain(Option) {
+        let downObj = this;
+        let tryConfig = downObj.tryAgainConfig;
+        Option.wait = tryConfig.wait;
+        Option.waitTimer = null;
+        console.log('downLoadImg：进入重试流程，等待时间，', Option.wait / 1000, 's');
 
-});
+        if (Option.runNum <= tryConfig.tryNum) {
+            if (Option.waitTimer === null) {
+                Option.waitTimer = global.setTimeout(() => {
+                    Option.runNum++;
+                    console.log('downLoadImg:文件 ' + Option.fileName + '尝试第：' + Option.runNum + '次重下');
+                    Option.wait = Option.wait + Option.wait;
+                    downObj.downMethod(Option);
+                }, Option.wait);
+            } else {
+                console.log('downLoadImg:文件 ' + Option.fileName + '已经创建等待任务');
+            }
+        } else {
+            console.log('downLoadImg:文件下载失败，已尝试：' + Option.runNum + '次重下');
+            Option.downState = 'faill'
+            Option.mainDownloadEnd(Option);
+        }
+
+    }
+}
 module.exports = downLoadMethod;
