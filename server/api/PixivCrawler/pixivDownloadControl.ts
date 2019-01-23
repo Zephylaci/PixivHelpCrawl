20e06b47868acc78803b3ab1fda0d0fb233b8362
Your Name <zephyru@163.com>
2019-01-23 19:24:31 +0800

    重构下载的接口逻辑

--- server/api/PixivCrawler/pixivDownloadControl.ts
+++ server/api/PixivCrawler/pixivDownloadControl.ts
@@ -1,59 +1,18 @@
-import {getPixivImgOriginal} from '../../service/getPixivImgOriginal';
+import { downList} from '../../service/getPixivImgOriginal';
 
-import {logger,loggerShow} from '../../utils/logger';
 var mainObj = {
-    common: {
-        runStat: false,
-    },
+
     contrl: async (ctx, next) => {
         ctx.body = {
             code: 200,
             content: '为啥没有返回值..'
         }
-        //这里可以重构
-        var common = mainObj.common;
         var data = JSON.parse(ctx.request.body.downList);
-        if (common.runStat === false) {
-            if (data.length != 0) {
-                common.runStat = true;
-                ctx.body.content = '本次云端下载已开始'
-                queryStart(data);
-            } else {
-                ctx.body.content = '云端已就绪'
-            }
-        } else {
-            if (data.length != 0) {
-                getPixivImgOriginal.addList(data);
-                ctx.body.content = '云端下载中，且已将本次提交添加至队列';
-            } else {
-                ctx.body.content = '云端下载中'
-            }
-        }
+        ctx.body.content = downList(data);
+
+        
     }
 }
 
-
-function queryStart(queryList) {
-    getPixivImgOriginal.downList(queryList).then((result:any) => {
-        let errIdArr = [];
-        loggerShow.info(`getPixivImgOriginal : 输出：${result.length}`);
-        result.forEach(element => {
-            if(element.state.indexOf('Err')!==-1){
-                loggerShow.warn(`downErr:${JSON.stringify(element)}`);
-                errIdArr.push(element.imgId);
-            }
-        });
-        if(errIdArr.length===0){
-            mainObj.common.runStat = false;
-        }else{
-            logger.warn('失败：',errIdArr.length);
-            logger.warn('pixivDownloadControl 存在下载出错的图片！10S后重试!');
-            setTimeout(()=>{
-                queryStart(errIdArr); 
-            },1000)
-        }
-        
-    });
-}
 
 export default mainObj
--- server/service/getPixivImgOriginal.ts
+++ server/service/getPixivImgOriginal.ts
@@ -1,25 +1,73 @@
 import { getProcess } from "../utils/processHandle";
-import { loggerShow } from "../utils/logger";
+import { loggerShow, logger, loggerErr } from "../utils/logger";
 
 
 let Process = getProcess({
     processAddr:'../service/process/getPixivImgProcess',
-    processName:'getPixivImgProcess',
-    waitTime:3000
+    processName:'getPixivImgProcess'
 });
+let COMMON={
+    runStat:false,
+    addItem:0,
+    step:0,
+    resList:[]
+}
 
-export let getPixivImgOriginal = {
-    /**
-     *  实际的调用入口 
-    */
-    downList: function (queryList) {
+export function downList(queryList){
+    if(queryList.length>0){
         loggerShow.info(`getPixivImgOriginal : 输入：${queryList.length}`);
-        return Process.send(queryList);
-    },
-    addList:function(queryList){
-        loggerShow.info(`getPixivImgOriginal : 追加输入：${queryList.length}`);
-        Process.send(queryList).then((res)=>{
-            loggerShow.info(`getPixivImgOriginal : 追加输出：${queryList.length}`);
+        COMMON.addItem+=queryList.length;
+        COMMON.step++;
+        Process.send(queryList).then((res:Array<any>)=>{
+            COMMON.resList = COMMON.resList.concat(res);
+            COMMON.step--;
+            if(COMMON.step===0){
+                checkOver();
+            }
         });
-    } 
+    }
+    if (COMMON.runStat === false) {
+        if (queryList.length != 0) {
+            COMMON.runStat = true;
+            return '本次云端下载已开始'
+        } else {
+            return'云端已就绪'
+        }
+    } else {
+        if (queryList.length != 0) {
+            return'云端下载中，且已将本次提交添加至队列';
+        } else {
+            return'云端下载中'
+        }
+    }
+
+}
+function checkOver(){
+    let result = COMMON.resList;
+    if(result.length!==COMMON.addItem){
+        loggerErr.error('getPixivOriginal:未被预料到的结束！');
+        loggerErr.error(JSON.stringify(COMMON));
+    }else{
+        let errIdArr = [];
+        loggerShow.info(`getPixivImgOriginal : 总输出：${result.length}`);
+        result.forEach(element => {
+            if(element.state.indexOf('Err')!==-1){
+                loggerShow.warn(`downErr:${JSON.stringify(element)}`);
+                errIdArr.push(element.imgId);
+            }
+        });
+        if(errIdArr.length===0){
+            loggerShow.info(`getPixivImgOriginal : 任务完成`);
+        }else{
+            logger.warn('失败：',errIdArr.length);
+            logger.warn('pixivDownloadControl 存在下载出错的图片！10S后重试!');
+            setTimeout(()=>{
+                downList(errIdArr); 
+            },10000)
+        }
+    }
+    COMMON.runStat = false;
+    COMMON.addItem = 0;
+    COMMON.step=0;
+    COMMON.resList=[];
 }
