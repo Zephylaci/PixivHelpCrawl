a968d4e4ffe9ac90f5e223a7123e95b23dc65193
Your Name <zephyru@163.com>
2019-01-25 20:48:05 +0800

    fixbug

--- server/middleware/generalResult.ts
+++ server/middleware/generalResult.ts
@@ -1,9 +1,12 @@
 import { resultBean } from "../utils/bean/resultBean";
 
-export function generalResult (ctx){
-    ctx.body = new resultBean({
-        code:100,
-        text:'连接成功'
-    });
+export function generalResult(ctx) {
+    if (ctx.request.method === 'POST') {
+        ctx.body = new resultBean({
+            code: 100,
+            text: '连接成功'
+        });
+    }
+
 }
 export default generalResult
